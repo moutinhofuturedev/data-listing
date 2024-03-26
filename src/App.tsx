f51420bae23497e1b0e050a4aaa82ce39b4959b7
paulo <paulo_vicali@icloud.com>
2024-03-25 23:10:28 -0300

    feature(master): adiciona componentes ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import { Plus } from 'lucide-react'
+import { FileDown, Plus, Search } from 'lucide-react'
+
+import { Header } from '@/components/header'
+import { Tabs } from '@/components/tabs'
+import { Button } from '@/components/ui/button'
+import { Control, Input } from '@/components/ui/input'
 
 export const App = () => {
   return (
-    <div className="py-10 space-y-8">
-      <div>header tabs</div>
-      <main className="max-w-6xl mx-auto space-y-5">
-        <div className="flex items-cente gap-3">
+    <div className="space-y-8 py-10">
+      <div>
+        <Header />
+        <Tabs />
+      </div>
+      <main className="mx-auto max-w-6xl space-y-5">
+        <div className="items-cente flex gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <button className="inline-flex items-center gap-1.5 text-xs font-medium bg-teal-300 text-teal-950 rounded-full px-1.5 py-1">
+          <Button variant="primary">
             <Plus className="size-3" />
             Create new
-          </button>
+          </Button>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <Input variant="filter">
+            <Search className="size-3" />
+            <Control placeholder="Search tags..." />
+          </Input>
+          <Button>
+            <FileDown className="size-3" />
+            Export
+          </Button>
         </div>
       </main>
     </div>
