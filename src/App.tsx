81227ab496a2bd694964aa0e7d86f177c56f95ae
paulo <paulo_vicali@icloud.com>
2024-03-30 10:58:25 -0300

    feature(main): adiciona tabela

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { FileDown, Plus, Search } from 'lucide-react'
+import { FileDown, MoreHorizontal, Plus, Search } from 'lucide-react'
 
 import { Header } from '@/components/header'
 import { Tabs } from '@/components/tabs'
 import { Button } from '@/components/ui/button'
 import { Control, Input } from '@/components/ui/input'
+
+import { Pagination } from './components/pagination'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './components/ui/table'
 
 export const App = () => {
   return (
@@ -31,6 +41,42 @@ export const App = () => {
             Export
           </Button>
         </div>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead></TableHead>
+              <TableHead>Tag</TableHead>
+              <TableHead>Amount of videos</TableHead>
+              <TableHead></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 10 }).map((_, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell></TableCell>
+                  <TableCell>
+                    <div className="flex flex-col gap-0.5">
+                      <span className="font-medium">React</span>
+                      <span className="text-xs text-zinc-500">
+                        b817fb26-1e04-43d1-8743-8df7027de9e4
+                      </span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-zinc-300">10 vídeo(s)</TableCell>
+                  <TableCell>
+                    <Button size="icon">
+                      <MoreHorizontal className="size-4" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+
+        <Pagination pages={5} items={10} page={1} />
       </main>
     </div>
   )
