9be6710dc52a04d1fbe36b5d86b0ece832409e3d
paulo <paulo_vicali@icloud.com>
2024-04-02 18:13:54 -0300

    feature(main): cria chamada da api

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import { keepPreviousData, useQuery } from '@tanstack/react-query'
 import { FileDown, MoreHorizontal, Plus, Search } from 'lucide-react'
+import { useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 import { Header } from '@/components/header'
 import { Tabs } from '@/components/tabs'
@@ -14,8 +17,36 @@ import {
   TableHeader,
   TableRow,
 } from './components/ui/table'
+import useDebounce from './hook/use-debounce'
+import { TagResponse } from './types'
 
 export const App = () => {
+  const [filter, setFilter] = useState('')
+  const [serchParams] = useSearchParams()
+
+  const debouncedFilter = useDebounce(filter, 1000)
+  const page = serchParams.get('page') ? Number(serchParams.get('page')) : 1
+
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ['get-tags', debouncedFilter, page],
+    queryFn: async () => {
+      const response = await fetch(
+        `http://localhost:3000/tags?_page=${page}&_per_page=10&title=${debouncedFilter}`,
+      )
+      const data: TagResponse = await response.json()
+
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+
+      return data
+    },
+    placeholderData: keepPreviousData,
+    staleTime: Infinity,
+  })
+
+  if (isLoading) {
+    return null
+  }
+
   return (
     <div className="space-y-8 py-10">
       <div>
@@ -34,7 +65,11 @@ export const App = () => {
         <div className="flex items-center justify-between">
           <Input variant="filter">
             <Search className="size-3" />
-            <Control placeholder="Search tags..." />
+            <Control
+              placeholder="Search tags..."
+              onChange={(event) => setFilter(event.target.value)}
+              value={filter}
+            />
           </Input>
           <Button>
             <FileDown className="size-3" />
@@ -52,19 +87,19 @@ export const App = () => {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {Array.from({ length: 10 }).map((_, index) => {
+            {tagsResponse?.data.map((tags) => {
               return (
-                <TableRow key={index}>
+                <TableRow key={tags.id}>
                   <TableCell></TableCell>
                   <TableCell>
                     <div className="flex flex-col gap-0.5">
-                      <span className="font-medium">React</span>
-                      <span className="text-xs text-zinc-500">
-                        b817fb26-1e04-43d1-8743-8df7027de9e4
-                      </span>
+                      <span className="font-medium">{tags.title}</span>
+                      <span className="text-xs text-zinc-500">{tags.id}</span>
                     </div>
                   </TableCell>
-                  <TableCell className="text-zinc-300">10 vídeo(s)</TableCell>
+                  <TableCell className="text-zinc-300">
+                    {tags.amountVideos} video(s)
+                  </TableCell>
                   <TableCell>
                     <Button size="icon">
                       <MoreHorizontal className="size-4" />
@@ -76,7 +111,13 @@ export const App = () => {
           </TableBody>
         </Table>
 
-        <Pagination pages={5} items={10} page={1} />
+        {tagsResponse && (
+          <Pagination
+            items={tagsResponse?.items}
+            page={page}
+            pages={tagsResponse?.pages}
+          />
+        )}
       </main>
     </div>
   )
---
+++ src/types.ts
@@ -0,0 +1,15 @@
+interface Tag {
+  title: string
+  amountVideos: number
+  id: string
+}
+
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
