1a07f2336f6293acd42455923d00d1dc971cc9e8
paulo <paulo_vicali@icloud.com>
2024-04-02 18:12:59 -0300

    feat(main): react query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import './index.css'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import { App } from './App'
 
+const queryClient = new QueryClient()
 const router = createBrowserRouter([
   {
     path: '/',
@@ -15,6 +17,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
